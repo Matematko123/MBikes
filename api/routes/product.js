b50b21b8d1d4a5c6913d7a707e4e540ed0f2ba94
Matematko123 <alilovicmatija2@gmail.com>
2022-03-27 20:35:34 +0200

    finished product route

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,5 +1,3 @@
-const CryptoJs = require('crypto-js');
-const { verify } = require('jsonwebtoken');
 const Product = require('../models/Product');
 
 const {
@@ -36,6 +34,49 @@ router.put('/:id', verifyTokenAndAdmin, async (req, res) => {
   } catch (error) {
     res.status(500).json(error);
   }
+});
+
+//DELETE
+router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json('Product has been deleted');
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET PRODUCT
+router.get('/find/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET PRODUCTS
+router.get('/', async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
+
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (qCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
+
+    res.status(200).json(products);
+  } catch (error) {}
 });
 
 module.exports = router;
