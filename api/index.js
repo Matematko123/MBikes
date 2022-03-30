b9ad27d9427560cef820bc21b9f94180b86f5f5b
Matematko123 <alilovicmatija2@gmail.com>
2022-03-30 18:18:37 +0200

    finished api/stripe routes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
+const cors = require('cors');
 
 const dotenv = require('dotenv');
 
@@ -14,10 +18,14 @@ mongoose
   .then(() => console.log('DB CONNECTED'))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 app.listen(process.env.PORT || 8000, () => {
   console.log('Backend server is running');
